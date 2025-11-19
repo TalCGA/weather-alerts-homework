5f3ed8597fdb21345aefc21a9f8a5a9857e6569f
Tal Chausho Gur Arie <talcga@Tals-MacBook-Pro.local>
2025-11-19 10:28:25 +0200

    backend - Add Tomorrow.io base URL to settings

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str = "change_me"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     TOMORROW_IO_API_KEY: str = "change_me"
+    TOMORROW_IO_BASE_URL: str = "https://api.tomorrow.io/v4"
 
     class Config:
         env_file = ".env"
